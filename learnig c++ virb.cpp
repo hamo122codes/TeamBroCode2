36ee9e5cce69ad7f8cd582c24ebfb0105d976032
mohamed ahmed <mandodeka@gmail.com>
2025-08-24 03:00:41 +0300

    Add files via upload

---
+++ learnig c++ virb.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	string name = "unknown";//
+	int age = 0;
+	double gpa = 0;
+	char letter = 'm';
+	bool success = true;
+	cout << "enter your name: ";
+	cin >> name;
+	cout << "enter your age: ";
+	cin >> age;
+	cout << "enter your gpa: ";
+	cin >> gpa;
+	cout << "enter your letter: ";
+	cin >> letter;
+
+	return 0;
+}
